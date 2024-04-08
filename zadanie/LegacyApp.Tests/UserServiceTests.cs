2d3f15a7d772a20d065a4dd1ea31aabf5fd2da64
Antoni Dawiowicz <s27366@pjwstk.edu.pl>
2024-04-08 13:43:58 +0200

    MOD testy jednostkowe

--- zadanie/LegacyApp.Tests/UserServiceTests.cs
+++ zadanie/LegacyApp.Tests/UserServiceTests.cs
@@ -5,10 +5,10 @@ public class UserServiceTests
     [Fact]
     public void AddUser_ReturnsFalseWhenFirstNameIsEmpty()
     {
-        // arange
+        // arange - wez zmienne, klasy, funkcje cokolwiek co potrzebujesz do wykonania funkcji testowej
         var userService = new UserService();
 
-        // act
+        // act - wykonaj funkcje
         var result = userService.AddUser(
             null,
             "nowak",
@@ -17,7 +17,7 @@ public class UserServiceTests
             1
         );
 
-        // assert
+        // assert - czy to co zwraca jest tym co chcesz
         Assert.False(result);
     }
     
@@ -38,6 +38,147 @@ public class UserServiceTests
         ); };
 
         // assert
+        Assert.Throws<ArgumentException>(action);
+    }
+
+    [Fact]
+    public void AddUser_ReturnsFalseWhenMissingAtSignAndDotInEmail()
+    {
+        //arange
+        var userService = new UserService();
+        
+        //act
+        var result = userService.AddUser(
+            "Karol",
+            "nowak",
+            "nowakkgmailcom",
+            DateTime.Parse("2000-01-01"),
+            1
+        );
+        
+        //assert
+        Assert.False(result);
+    }
+    
+    [Fact]
+    public void AddUser_ReturnsFalseWhenYoungerThen21YearsOld()
+    {
+        //arange
+        var userService = new UserService();
+        
+        //act
+        var result = userService.AddUser(
+            "Karol",
+            "nowak",
+            "nowakk@gmail.com",
+            DateTime.Parse("2004-01-01"),
+            1
+        );
+        
+        //assert
+        Assert.False(result);
+    }
+    
+    [Fact]
+    public void AddUser_ReturnsTrueWhenVeryImportantClient()
+    {
+        //arange
+        var userService = new UserService();
+        var cr = new ClientRepository();
+
+        cr.GetById(1).Type = "VeryImportantClient";
+        
+        //act
+        var result = userService.AddUser(
+            "Karol",
+            "nowak",
+            "nowakk@gmail.com",
+            DateTime.Parse("2001-01-01"),
+            1
+        );
+        
+        //assert
+        Assert.True(result);
+    }
+    
+    [Fact]
+    public void AddUser_ReturnsTrueWhenImportantClient()
+    {
+        //arange
+        var userService = new UserService();
+
+        
+        //act
+        var result = userService.AddUser(
+            "Karol",
+            "Smith",
+            "nowakk@gmail.com",
+            DateTime.Parse("2001-01-01"),
+            3
+        );
+        
+        //assert
+        Assert.True(result);
+    }
+    
+    [Fact]
+    public void AddUser_ReturnsTrueWhenNormalClient()
+    {
+        //arange
+        var userService = new UserService();
+
+        
+        //act
+        var result = userService.AddUser(
+            "Karol",
+            "Kwiatkowski",
+            "nowakk@gmail.com",
+            DateTime.Parse("2001-01-01"),
+            5
+        );
+        
+        //assert
+        Assert.True(result);
+    }
+    
+    [Fact]
+    public void AddUser_ReturnsFalseWhenNormalClientWithNoCreditLimit()
+    {
+        //arange
+        var userService = new UserService();
+
+        
+        //act
+        var result = userService.AddUser(
+            "Karol",
+            "Andrzejewicz",
+            "nowakk@gmail.com",
+            DateTime.Parse("2001-01-01"),
+            6
+        );
+        
+        //assert
+        Assert.False(result);
+    }
+    
+    [Fact]
+    public void AddUser_ThrowsExceptionWhenUserNoCreditLimitExistsForUser()
+    {
+        //arange
+        var userService = new UserService();
+
+        
+        //act
+        Action action = () => {var result = userService.AddUser(
+            "Karol",
+            "Andrzejewicz",
+            "nowakk@gmail.com",
+            DateTime.Parse("2001-01-01"),
+            6
+        ); };
+
+        
+        //assert
         Assert.Throws<ArgumentException>(action);
     }
 }
